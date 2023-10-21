a68604b45ff657afbefef37dd0172cdbfbdbc626
lolamtisch <lolamtisch@users.noreply.github.com>
2023-10-21 05:07:27 +0000

    Auto Urls

--- src/pages/playerUrls.js
+++ src/pages/playerUrls.js
@@ -657,6 +657,7 @@ module.exports = {
       '*://valeronevijao.com/e/*',
       '*://strawberriesporail.com/e/*',
       '*://timberwoodanotia.com/e/*',
+      '*://nectareousoverelate.com/e/*',
       // auto-voe-replace-dont-remove
     ],
   },
